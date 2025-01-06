128f6b4025a5892bbaaff3ae77b87a8dbdd00e7d
Tushar Kumain <dcm.tusharmi@gmail.com>
2025-01-06 20:09:08 +0530

    Implement to-do list functionality with add, display, and remove features

--- practice.js
+++ practice.js
@@ -1,11 +1,32 @@
-let promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("hello");
-    }, 3000);
-  });
-  
-  promise.then((value) => {
-    console.log(value); // Output: "hello" after 3 seconds
-  }).catch((error) => {
-    console.log("Error:", error);
-  });
+let todos = [];
+
+// Function to add a new to-do
+function addTodo(todo) {
+    todos.push(todo);
+    console.log(`Added: ${todo}`);
+}
+
+// Function to display all to-dos
+function displayTodos() {
+    console.log("To-Do List:");
+    todos.forEach((todo, index) => {
+        console.log(`${index + 1}. ${todo}`);
+    });
+}
+
+// Function to remove a to-do by index
+function removeTodo(index) {
+    if (index >= 0 && index < todos.length) {
+        const removed = todos.splice(index, 1);
+        console.log(`Removed: ${removed[0]}`);
+    } else {
+        console.log("Invalid index");
+    }
+}
+
+// Example usage
+addTodo("Learn JavaScript");
+addTodo("Build a to-do app");
+displayTodos();
+removeTodo(0);
+displayTodos();
